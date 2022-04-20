91bc9cffb13b300cc690e8aab1da1807403ef7d3
Lord-Chaos-YT <thumper540@gmail.com>
2022-04-20 17:59:39 -0600

    Some changes

--- game-modules/scrolling_map_h.hpp
+++ game-modules/scrolling_map_h.hpp
@@ -2,11 +2,26 @@
 #include "game-modules/forward_decls.hpp"
 #include "sfml-ui/UI/concrete.hpp"
 
+#define SFML_STATIC
+
 namespace game {
+    class Tile : public sf::RectangleShape {
+    private:
+        bool solid;
+
+        virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const {
+            target.draw(, states);
+        }
+    public:
+        Tile(const sf::Vector2f& pos, const sf::Vector2f& size, bool isSolid = true);
+    };
+
     class ScrollingMap {
     private:
         matrix<int> tileMap;
     public:
         ScrollingMap(sf::Vector2u dim);
+
+        matrix<Tile> loadScreen() {}
     };
 };
